975e2c2694e1b439c20eab961143680340cb14ff
Sherry Martin <sm674@cornell.edu>
2018-11-03 20:49:21 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,20 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero_power = []
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @whole_team = params[:team][:members]
+      @whole_team.each do |i_hero|
+      @hero_name << i_hero[:name]
+      @hero_power << i_hero[:power]
+      @hero_bio << i_hero[:bio]
+
+
+
       @team = Team.new(params[:team][:name], params[:team][:motto])
 
       params[:team][:members].each do |content|
